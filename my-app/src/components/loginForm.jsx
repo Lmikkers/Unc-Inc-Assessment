ad3d61e74bbad5fb19e71c9f5f2c48bfe2b6a3c6
Larissa <larissa.mikkers@hotmail.com>
2024-12-08 21:09:42 +0100

    User Auth toegevoegd
    Inloggen werkt! Redirect naar dashboard, alleen nog ervoor zorgen dat er een log uit button komt

--- my-app/src/components/loginForm.jsx
+++ my-app/src/components/loginForm.jsx
@@ -1,56 +1,44 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from "react-router-dom";
+import { useAuth } from '../AuthContext';
 
-export function LoginForm({ login }) {
+export function LoginForm() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const { login } = useAuth();
     const navigate = useNavigate();
 
     const handleSubmit = (event) => {
         event.preventDefault();
         if (username === 'uncinc' && password === 'letmein') {
-            login(username, password);
-            setIsLoggedIn(true); // Zet de status op ingelogd
+            login(username); // Stel gebruiker in
+            navigate("/dashboard"); // Redirect naar dashboard
         } else {
             alert('Verkeerde gegevens');
         }
     };
 
-    // Redirect naar dashboard als de gebruiker is ingelogd
-    useEffect(() => {
-        if (isLoggedIn) {
-            navigate("/dashboard");
-        }
-    }, [isLoggedIn, navigate]);
-
     return (
-        <div>
-            {isLoggedIn ? (
-                <button onClick={() => setIsLoggedIn(false)}>Uitloggen</button>
-            ) : (
-                <form onSubmit={handleSubmit}>
-                    <label htmlFor="username">Gebruikersnaam</label>
-                    <input
-                        type="text"
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                        id="username"
-                        placeholder="Gebruikersnaam"
-                        required
-                    />
-                    <label htmlFor="password">Wachtwoord</label>
-                    <input
-                        type="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        id="password"
-                        placeholder="Wachtwoord"
-                        required
-                    />
-                    <button type="submit">Login</button>
-                </form>
-            )}
-        </div>
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="username">Gebruikersnaam</label>
+            <input
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                id="username"
+                placeholder="Gebruikersnaam"
+                required
+            />
+            <label htmlFor="password">Wachtwoord</label>
+            <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                id="password"
+                placeholder="Wachtwoord"
+                required
+            />
+            <button type="submit">Login</button>
+        </form>
     );
 }
--- my-app/src/utils/ProtectedRoutes.jsx
+++ my-app/src/utils/ProtectedRoutes.jsx
@@ -1,7 +1,9 @@
 import { Outlet, Navigate } from "react-router-dom";
+import { useAuth } from "../AuthContext";
+
 
 const ProtectedRoutes = () => {
-    const user = null
+    const { user } = useAuth();
     return user ? <Outlet/> : <Navigate to="/login"/>
 }
 
